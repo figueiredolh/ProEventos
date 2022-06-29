b534843a78462e7d5721ffb28f7d07fc2e5fdf8a
Henrique Figueiredo <henricklima94@gmail.com>
2022-06-29 17:22:32 -0300

    First commit - Seção 2 .NET Core 5

---
+++ Back/src/ProEventos.api/Controllers/ProEventosController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProEventos.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProEventosController : ControllerBase
+    {
+        private Evento[] _eventos = new Evento[]{
+            new Evento()
+            {
+                Id = 1,
+                Name = "Aniversário",
+                Date = DateTime.Now
+            },
+            new Evento()
+            {
+                Id = 2,
+                Name = "Jogo do Paysandu",
+                Date = DateTime.Now
+            },
+            new Evento()
+            {
+                Id = 3,
+                Name = "Trabalho",
+                Date = DateTime.Now
+            }
+        };
+
+        [HttpGet]
+        [Route("eventos")]
+        //Get para retorno da coleção de Eventos
+        public IEnumerable<Evento> GetEventos()
+        {
+            return _eventos;
+        }
+
+        //Get para retorno de Evento por Id
+        [HttpGet]
+        [Route("eventos/{id:int}")]
+        public Evento GetEventoById(int id)
+        {
+            var evento = _eventos.Where(e => e.Id == id).FirstOrDefault();
+            return evento;
+        }
+    }
+}
---
+++ Back/src/ProEventos.api/Models/Evento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.api.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
